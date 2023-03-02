b6f801efa40fd972132b4d3971084012e52f8468
Bugfix Jacques <jacques@iconica.nl>
2023-03-02 11:00:57 +0100

    feat: changed some parameters for cookie bar

--- frontend/index.php
+++ frontend/index.php
@@ -253,7 +253,7 @@ $docu_menu = array(
 	<![endif]-->
 
 <script src='https://www.google.com/recaptcha/api.js' async defer></script>
-<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/cookie-bar/cookiebar-latest.min.js?forceLang=nl&always=1&top=1&hideDetailsBtn=1&showPolicyLink=1&privacyPage=https%3A%2F%2Fwww.meteobase.nl%2Fmeteobase%2Fdownloads%2Ffixed%2Fliteratuur%2F09022023_HWH_privacyverklaring.pdf"></script>
+<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/cookie-bar/cookiebar-latest.min.js?forceLang=nl&tracking=1&always=1&top=1&showNoConsent=1&hideDetailsBtn=1&showPolicyLink=1&privacyPage=https%3A%2F%2Fwww.meteobase.nl%2Fmeteobase%2Fdownloads%2Ffixed%2Fliteratuur%2F09022023_HWH_privacyverklaring.pdf"></script>
 
 <script type="text/javascript">
 function melding() { document.getElementById('dl').innerHTML='<p>Uw bestelling wordt verwerkt. U ontvangt binnen een enkele uren een e-mail<br />met een downloadlink. U mag dit tabblad verlaten.</p>'; $('#downloadbutton').fadeOut('fast'); }

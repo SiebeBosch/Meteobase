a807abf9ab86638a873d33e4392717df55d24bc5
Siebe Bosch <siebe@hydroconsult.nl>
2024-01-17 11:44:31 +0100

    Corrected path for new server

--- backend/WIWBHERHALINGSTIJD/WIWBHERHALINGSTIJD/HERHALINGSTIJD.vb
+++ backend/WIWBHERHALINGSTIJD/WIWBHERHALINGSTIJD/HERHALINGSTIJD.vb
@@ -33,7 +33,7 @@ Module HERHALINGSTIJD
                 GDALDemPath = "c:\Program Files\GDAL\gdaldem.exe"
             Else
                 HERH.RasterViewURL = "https://www.meteobase.nl/images/rasterviews/"                                               'laptop en server
-                HERH.RasterViewDIR = "c:\Program Files (x86)\PostgreSQL\EnterpriseDB-ApachePHP\apache\www\images\rasterviews\"  'server
+                HERH.RasterViewDIR = "C:\Apache24\htdocs\images\rasterviews\"  'server
                 HERH.DownloadURL = "https://www.meteobase.nl/meteobase/downloads/"                                                 'laptop en server
                 HERH.DownloadDIR = "c:\Apache24\htdocs\meteobase\downloads\"  'server
                 HERH.TempDir = "c:\temp\"
